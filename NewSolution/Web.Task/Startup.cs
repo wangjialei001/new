381a4590ba27a885d3bd95e5788913ac4b75826c
wangjialei <wangjialei@gwjia.com.cn>
2019-08-14 16:27:44 +0800

    test

--- NewSolution/Web.Task/Startup.cs
+++ NewSolution/Web.Task/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Hangfire;
+﻿using Hangfire;
 using Hangfire.SqlServer;
 using Infrastructure.SyncData;
 using Microsoft.AspNetCore.Builder;
@@ -10,8 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using System;
 using Web.Tasks.MessageTask;
 
 namespace Web.Tasks
